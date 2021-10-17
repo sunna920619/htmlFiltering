e2fb01340057f41b31500865227eb2d4b11fb9d2
haena.lee <haena.lee@bankwareglobal.com>
2021-10-17 20:28:59 +0900

    feat : SpringApplication 파일 커밋

---
+++ htmlFiltering/src/main/java/com/wemakeprice/htmlfiltering/HtmlFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.wemakeprice.htmlfiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlFilteringApplication.class, args);
+	}
+
+}
---
+++ htmlFiltering/src/test/java/com/wemakeprice/htmlfiltering/HtmlFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemakeprice.htmlfiltering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlFilteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
